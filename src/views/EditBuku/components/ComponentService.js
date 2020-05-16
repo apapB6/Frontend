093a1067a397dc2ec78eed7deac43ef4282ee761
dwianaambarr <dwianaambarr@gmail.com>
2020-05-16 15:19:09 +0700

    edit peminjaman dan buku fixed

--- src/views/EditBuku/components/ComponentService.js
+++ src/views/EditBuku/components/ComponentService.js
@@ -7,7 +7,7 @@ class ComponentService {
 	editBuku(bukuId) {
 		return axios.get(`${EDITBUKU_API_URL}` + bukuId)
 	}
-	editBukuPost(bukuId){
+	editBukuPost(bukuId, buku){
 		return axios.post(`${EDITBUKU_API_URL}` + bukuId, buku)
 	}
 }
